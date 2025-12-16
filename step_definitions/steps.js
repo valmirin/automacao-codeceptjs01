ef5baf39c5a62afdbf8a6aa61f6d9916a3d10331
Valmir Filho <valmirin17@hotmail.com>
2025-12-16 16:28:14 -0300

    Edit login page name in steps.js

--- step_definitions/steps.js
+++ step_definitions/steps.js
@@ -1,20 +1,20 @@
-const { I, login_page, home_page } = inject();
+const { I, page_login, home_page } = inject();
 // Add in your custom step files
 
 Given("I fill email", () => {
   // From "features\basic.feature" {"line":7,"column":5}
-  I.waitForElement(login_page.fields.email, 20);
-  I.fillField(login_page.fields.email, "teste@teste.com");
+  I.waitForElement(page_login.fields.email, 20);
+  I.fillField(page_login.fields.email, "teste@teste.com");
 });
 
 Given("I fill password", () => {
   // From "features\basic.feature" {"line":8,"column":5}
-  I.fillField(login_page.fields.password, "123456");
+  I.fillField(page_login.fields.password, "123456");
 });
 
 When("I tap on Entrar", () => {
   // From "features\basic.feature" {"line":9,"column":5}
-  I.tap(login_page.buttons.enter);
+  I.tap(page_login.buttons.enter);
 });
 
 Then("I see the search field", () => {
